584b579f6b140ff93d0f90f66d064efc6b1cc698
nonameirl <jeremycagle@gmail.com>
2022-10-01 21:00:16 -0700

    rolling commits

--- test.py
+++ test.py
@@ -3,17 +3,21 @@ from myimports import *
 for my_zip in glob.glob('*.zip'):
     print("________________________________________")
 
-    data_dir = Path(my_zip).stem
-    data_wd = EXTRACT_DIR.joinpath(data_dir)
 
-    print("\n    " + data_dir + " election data\n")
+    # Define working data folder for current election
+    data_wd = EXTRACT_DIR.joinpath(Path(my_zip).stem)
+
+    # parse the Readme.txt file to
+    get_location(my_zip)
 
     # Extract zip file
     with zipfile.ZipFile(my_zip) as zip_file:
         zip_file.extractall(data_wd)
         zip_file.close()
 
-    get_location(my_zip)
+
+
+
 
 print("________________________________________")
 
